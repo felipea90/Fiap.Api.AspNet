7beb8c87ea9587b9d84bcf2824c0cb6615b5b38f
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-09-13 15:45:40 -0300

    Produto

--- Fiap.Api.AspNet/Controllers/ProdutoController.cs
+++ Fiap.Api.AspNet/Controllers/ProdutoController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Fiap.Api.AspNet.Models;
+using Fiap.Api.AspNet.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,6 +14,96 @@ namespace Fiap.Api.AspNet.Controllers
     [ApiController]
     public class ProdutoController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IList<ProdutoModel>> GetAll(
+            [FromServices] IProdutoRepository produtoRepository)
+        {
+            var produto = produtoRepository.FindAll();
 
+            if (produto.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(produto);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<ProdutoModel> GetById(
+            [FromRoute] int id,
+            [FromServices] IProdutoRepository produtoRepository)
+        {
+            var produto = produtoRepository.FindById(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public ActionResult<ProdutoModel> Post(
+            [FromServices] IProdutoRepository produtoRepository,
+            [FromBody] ProdutoModel produtoModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var categoriaId = produtoRepository.Insert(produtoModel);
+                produtoModel.ProdutoId = categoriaId;
+
+                var location = new Uri(Request.GetEncodedUrl() + categoriaId);
+
+                return Created(location, produtoModel);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new { message = $"Não foi possível inserir a marca. Detalhes: {error.Message}" });
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<CategoriaModel> Put(
+            [FromRoute] int id,
+            [FromServices] IProdutoRepository produtoRepository,
+            [FromBody] ProdutoModel produtoModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (produtoModel.ProdutoId != id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                produtoRepository.Update(produtoModel);
+
+                return Ok(produtoModel);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new { message = $"Não foi possível alterar a marca. Detalhes: {error.Message}" });
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<CategoriaModel> Delete(
+            [FromRoute] int id,
+            [FromServices] IProdutoRepository produtoRepository)
+        {
+            produtoRepository.Delete(id);
+
+            return Ok();
+        }
     }
 }
